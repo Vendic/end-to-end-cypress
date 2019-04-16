ae5df2a0dfd53850e73dc291b5092b8f8cb302cd
Robbert Gijsberts <robbert@vendic.nl>
2019-04-16 16:48:42 +0200

    added structure to the test

--- cypress/integration/checkout_flow_spec.js
+++ cypress/integration/checkout_flow_spec.js
@@ -2,7 +2,7 @@ const baseURL = 'https://staging.plasmavisie.nl/'
 
 describe('Test the checkout flow', function() {
 
-    it('Should navigate to a product, add it to the cart and remove it', function() {
+    it('Should navigate to a product', function(){
         cy.visit(baseURL)
         
         cy.get('.navigation')
@@ -10,18 +10,27 @@ describe('Test the checkout flow', function() {
             .get('ul li.nav-1 a')
             .click()
             .url().should('contain', '/led-televisie')
-            .get('.products-grid .product-items .product-item').eq(1)
-            .find('a.product').click()
-            .url().should('contain', 'qe65q9fn')
-            .get('#product-addtocart-button').should('be.visible')
-            .wait(2000) 
-            .click()           
-            .get('.page.messages [data-bind="html: message.text"] a').should('contain', 'je winkelmand').click()
-            .url().should('contain', '/cart') 
-            // .get('.cart.item .product-item-name a').should('have.attr', 'href').and('contain', 'qe65q9fn')
-            // .get('[data-role="cart-item-qty"]').clear().type('3').get('button[data-cart-item-update]').click()
-            // .get('[data-role="cart-item-qty"]').should('have.value', '3')
-            // .get('.cart.item .photo.image').should('have.attr', 'src').should('contain', '/media/catalog/')
+    })
+
+    it('Clicks on a product and visits the product detail page and add product to cart', function(){
+        cy.get('.products-grid .product-items .product-item').eq(1)
+        .find('a.product').click()
+        .url().should('contain', 'qe65q9fn')
+        .get('#product-addtocart-button').should('be.visible')
+        .wait(2000) 
+        .click()
+        cy.get('.page.messages [data-bind="html: message.text"] a').should('contain', 'je winkelmand').click()
+        .url().should('contain', '/cart') 
+    })
+
+ 
+
+    it('Should navigate to a product, add it to the cart and remove it', function() {                       
+            
+            cy.get('.cart.item .product-item-name a').should('have.attr', 'href').and('contain', 'qe65q9fn')
+            .get('[data-role="cart-item-qty"]').clear().type('3').wait(1000).get('button[data-cart-item-update]').click()
+            .get('[data-role="cart-item-qty"]').should('have.value', '3')
+            .get('.cart.item .photo.image').should('have.attr', 'src').should('contain', '/media/catalog/')
             .get('.minicart-wrapper .showcart').click()
             .get('.block-minicart #mini-cart .product-image-photo').should('have.attr', 'src').should('contain', '/media/catalog')
             .get('.block-minicart #mini-cart .minicart-price').contains(/(\d+\,|.\d{1,2})/).and('be.visible')
@@ -30,7 +39,6 @@ describe('Test the checkout flow', function() {
             .get('.block-minicart #mini-cart .cart-item-qty').should('have.value', '3')
 
 
-            // .get('.minicart-wrapper .showcart').click()
             .get('.block-minicart .viewcart').should('be.visible').click().url().should('contain', '/cart')
             .wait(2000)
             .get('.minicart-wrapper .showcart').click()
